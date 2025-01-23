f98895c3235c402f4815cbad11cc8b9640a65431
ViktorNoskire <vikkerinho@gmail.com>
2025-01-23 16:17:31 +0100

    Change styling on buttons in single movie page and fix an error

--- static/eventListeners.js
+++ static/eventListeners.js
@@ -1,27 +1,28 @@
 // ____________________________________________________
+if (document.querySelector('.movie-headline')) {
+  const item0 = document.querySelector('.modal-item-0');
+  const item1 = document.querySelector('.modal-item-1');
+  const item2 = document.querySelector('.modal-item-2');
 
-const item0 = document.querySelector('.modal-item-0');
-const item1 = document.querySelector('.modal-item-1');
-const item2 = document.querySelector('.modal-item-2');
+  function addListener(li) {
+    const btn = li.querySelector('.modal-open');
+    const answer = li.querySelector('.modal-answer');
 
-function addListener(li) {
-  const btn = li.querySelector('.modal-open');
-  const answer = li.querySelector('.modal-answer');
+    btn.addEventListener('click', () => {
+      btn.classList.toggle('clicked');
+      if (btn.className === 'modal-open clicked') {
+        btn.src = './img/QnAClose.png';
+        btn.alt = 'Close button';
+        answer.style.display = '';
+      } else {
+        btn.src = './img/QnAOpen.png';
+        btn.alt = 'Open button';
+        answer.style.display = 'none';
+      }
+    });
+  }
 
-  btn.addEventListener('click', () => {
-    btn.classList.toggle('clicked');
-    if (btn.className === 'modal-open clicked') {
-      btn.src = './img/QnAClose.png';
-      btn.alt = 'Close button';
-      answer.style.display = '';
-    } else {
-      btn.src = './img/QnAOpen.png';
-      btn.alt = 'Open button';
-      answer.style.display = 'none';
-    }
-  });
+  addListener(item0);
+  addListener(item1);
+  addListener(item2);
 }
-
-addListener(item0);
-addListener(item1);
-addListener(item2);
